9723156fe44f053a6c6f9f7f98237b4db99994c6
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2022-05-16 10:09:58 +0530

    Balanced Parantheses!

---
+++ InterviewBit/Balanced Parantheses!.cpp
@@ -0,0 +1,12 @@
+int Solution::solve(string A) {
+    stack<char> s;
+    for(int i=0; i<A.size(); i++) {
+        if(A[i] == '(') s.push(A[i]);
+        else {
+            if(s.empty()) return 0;
+            else s.pop();
+        }
+    }
+
+    return s.empty() ? 1 : 0;
+}
