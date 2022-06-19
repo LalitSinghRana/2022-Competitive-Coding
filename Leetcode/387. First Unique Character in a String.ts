44a4302fa706ed1c6819b74f8b4126d75d2b4505
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2022-06-19 20:09:28 +0530

    387. First Unique Character in a String

---
+++ Leetcode/387. First Unique Character in a String.ts
@@ -0,0 +1,12 @@
+function firstUniqChar(s: string): number {
+    let m = new Map<string, number>();
+    for(let i=0; i<s.length; i++) {
+        m.set(s[i], (m.get(s[i]) ?? 0) +1);
+    }
+    
+    for(let i=0; i<s.length; i++) {
+        if(m.get(s[i]) === 1) return i;
+    }
+    
+    return -1;
+};
