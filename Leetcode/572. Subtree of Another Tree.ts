f7e46cb88ddc44fea09af7fe24f4399733201b7d
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2022-07-09 21:14:59 +0530

    572. Subtree of Another Tree

---
+++ Leetcode/572. Subtree of Another Tree.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    if(!subRoot) return true
+    else if(!root) return false;
+    
+    return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
+
+let ap: Number[] = [];
+let aq: Number[] = [];
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    ap = [];
+    aq = [];
+    
+    dfs(p, ap);
+    dfs(q, aq);
+    
+    if(ap.length !== aq.length) return false;
+    for(let i=0; i<ap.length; i++) {
+        if(ap[i] !== aq[i]) return false;
+    }
+    return true;
+};
+
+const dfs = (r: TreeNode, arr: Number[]): void => {
+    if(!r) {
+        arr.push(null)
+        return;
+    }
+    arr.push(r.val);
+    dfs(r.left, arr);
+    dfs(r.right, arr);
+}
