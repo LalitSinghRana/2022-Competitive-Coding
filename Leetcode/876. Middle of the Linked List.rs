54e0cf2e771dd991014a6d1335b78f7c84ef1717
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2022-09-23 18:51:34 +0530

    876. Middle of the Linked List.rs

---
+++ Leetcode/876. Middle of the Linked List.rs
@@ -0,0 +1,28 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let (mut s, mut f) = (&head, &head);
+        
+        while f.is_some() && f.as_ref().unwrap().next.is_some() {
+            s = &(s.as_ref().unwrap().next);
+            f = &(f.as_ref().unwrap().next.as_ref().unwrap().next);
+        }
+        
+        s.clone()
+    }
+}
