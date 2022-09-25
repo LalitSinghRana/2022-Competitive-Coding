9a52ae21a1c36818ef80bc590e415594a6b12e7e
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2022-09-25 17:52:56 +0530

    1342. Number of Steps to Reduce a Number to Zero.rs

---
+++ Leetcode/1342. Number of Steps to Reduce a Number to Zero.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn number_of_steps(num: i32) -> i32 {
+        let mut cnt = 0;
+        let mut n = num;
+        while(n != 0) {
+            if(n%2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+            }
+            cnt += 1;
+        }
+        
+        cnt
+    }
+}
