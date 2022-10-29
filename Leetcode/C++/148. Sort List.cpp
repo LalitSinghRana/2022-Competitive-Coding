94fbb119b79e8ba79d2834e0376967a9771227fd
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2022-10-30 00:47:45 +0530

    148. Sort List.cpp

---
+++ Leetcode/C++/148. Sort List.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> v;
+        
+        auto cur = head;
+        while(cur) {
+            v.push_back(cur->val);
+            cur = cur->next;
+        }
+        
+        sort(v.begin(), v.end());
+        
+        cur = head;
+        for(auto x : v) {
+            cur->val = x;
+            cur = cur->next;
+        }
+        
+        return head;
+    }
+};
