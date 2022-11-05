936cd92be3900de2dd01fb2395aac7efba1f2276
Lalit Singh Rana <31415645+LalitSinghRana@users.noreply.github.com>
2022-11-05 21:44:43 +0530

    48. Rotate Image.cpp

--- Leetcode/C++/48. Rotate Image.cpp
+++ Leetcode/C++/48. Rotate Image.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& A) {
-        int n = A.size();
+        reverse(A.begin(), A.end());
         
-        for(int i=0; i<n; i++) {
-            auto l = 0, r = i;
-            while(l < r) {
-                swap(A[r][l], A[l][r]);
-                l++; r--;
-            }
-            
-            l = i;
-            r = n-1;
-            while(i && l<r) {
-                swap(A[r][l], A[l][r]);
-                l++; r--;
-            }
+        for(int i=0; i<A.size(); i++) {
+            for(int j=0; j<i; j++) swap(A[i][j], A[j][i]);
         }
-        
-        for(auto &v : A) reverse(v.begin(), v.end());
     }
 };
